fa9a13828ec79b48612130f5ec4238b43fb05092
Max <sewemaxy@gmail.com>
2023-07-02 15:04:39 +0300

    assignment_2

---
+++ Python_conditions.py
@@ -0,0 +1,19 @@
+marks = int(input("please enter marks:"))
+
+
+if (marks>=80):
+    grade="A Grade"
+
+elif(marks>=60):
+    grade="B Grade"
+
+elif(marks>=40):
+    grade="C Grade"
+
+elif (marks>=0):
+    grade="D Grade"
+
+else: (marks>100)
+
+print("Marks:", marks)
+print("Grade:", grade)
